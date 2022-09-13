9494da2c909f90d2a5ba121bddf58f11aa73def3
Alongedaniel <alongedaniel41@gmail.com>
2022-09-13 04:15:25 -0700

    Added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "_putchar.c"
+
+/**
+ * main - Prints _putchar.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_putchar();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int print_putchar(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
