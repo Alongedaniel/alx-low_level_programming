aa095bbac639d81149f94269a0039a2dc9101450
Alongedaniel <alongedaniel41@gmail.com>
2022-09-13 05:41:38 -0700

    Added task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,18 @@
-#include <main.h>
+#include <stdio.h>
 
 /**
  * print_alphabet - Prints Alphabets.
  *
  * Return: Always 0.
  */
+
+void print_alphabet();
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 
 void print_alphabet(void)
 {
