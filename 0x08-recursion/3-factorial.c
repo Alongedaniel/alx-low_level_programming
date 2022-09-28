de85068e24bc3ce12d99e3c6ad2be728c5d18cba
Alongedaniel <alongedaniel41@gmail.com>
2022-09-28 07:20:28 -0700

    Added task 0-4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * factorial - check the code
+ * @n: variable
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
