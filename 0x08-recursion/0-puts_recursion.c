4f7bc468f4fd2778ca7fdf40bf5041f1c340ab19
Alongedaniel <alongedaniel41@gmail.com>
2022-09-28 15:14:25 -0700

    Added task 5-8

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * _puts_recursion - check the code
- * @s: variable.
- * Return: Always 0.
- */
+  * _puts_recursion - prints characters in a string
+  * @s: string to be printed
+  */
 
 void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
+		_putchar('\n');
 		return;
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-	}
-	_putchar('\n');
+
 }
