421cba35d786ab184cf14489c40e2c80c7e5f8a4
Alongedaniel <alongedaniel41@gmail.com>
2022-10-12 14:00:15 -0700

    Added task 4

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,12 +19,9 @@ int main(int ac, char *av[])
 		printf("Error\n");
 		exit(98);
 	}
-	if(av[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+
 	f = get_op_func(av[2]);
+
 	if (f == NULL)
 	{
 		printf("Error\n");
