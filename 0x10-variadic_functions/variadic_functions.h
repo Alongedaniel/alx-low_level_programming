011433dda7ed87040a5c9c52869f1d17f64c79d2
Alongedaniel <alongedaniel41@gmail.com>
2022-10-13 03:55:19 -0700

    Added task 0-2

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <stddef.h>
+
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
