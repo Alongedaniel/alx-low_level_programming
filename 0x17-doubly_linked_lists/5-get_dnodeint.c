9e5eae0758cf76bf0a799309e62ccd20b2af3751
Alongedaniel <alongedaniel41@gmail.com>
2022-12-08 12:47:53 -0800

    Added task 2-5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,13 +13,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (head != NULL)
 	{
-	  if (i == index)
-	  {
-		  return (head);
-	  }
+		if (i == index)
+		{
+			return (head);
+		}
 
-	  head = head->next;
-	  i++;
+		head = head->next;
+		i++;
 	}
 
 	return (NULL);
